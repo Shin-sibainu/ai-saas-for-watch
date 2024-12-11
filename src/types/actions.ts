4548f25500af9d2d4c0f07d04f50cd0a14722b73
Shin-sibainu <shincode0712@gmail.com>
2024-12-11 09:12:27 +0900

    first commit

---
+++ src/types/actions.ts
@@ -0,0 +1,13 @@
+export type GenerateImageState = {
+  imageUrl?: string;
+  error?: string;
+  status: "idle" | "error" | "success";
+  keyword?: string;
+};
+
+export type RemoveBackgroundState = {
+  originalImage?: string;
+  processedImage?: string;
+  status: "idle" | "error" | "success";
+  error?: string;
+};
